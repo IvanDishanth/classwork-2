d3f9d12e927625c31aca0ca5319212990ed9ffab
Dishanth Ivan <ivandishanth@gmail.com>
2025-03-13 23:29:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function ge1(){
+    document.getElementById("round").style.backgroundColor="red";
+}
+function ge2() {
+    document.getElementById("round").style.backgroundColor="green";
+}
+function ge3() {
+    document.getElementById("round").style.backgroundColor="yellow";
+}
