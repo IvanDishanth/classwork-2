5cb09a452af0e0d1ed6f4bb892c5c7d5cb2cdce1
Dishanth Ivan <ivandishanth@gmail.com>
2025-03-14 19:00:50 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function on(){
-    document.getElementById("l1").src="images/balb (1)(1).jpg";
+    document.getElementById("l1").src="images/balb (2).jpg";
 }
 function off(){
-    document.getElementById("l1").src="images/balb (2).jpg";
+    document.getElementById("l1").src="images/balb (1)(1).jpg";
 }
